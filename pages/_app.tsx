46750e120d21e412bb2076d59c47991a960f0d15
Stefanie <stefcodes@gmail.com>
2023-01-23 20:15:32 -0700

    change testnet chain

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import ThirdwebGuideFooter from "../components/ThirdwebGuideFooter";
 import Header from "../components/Header";
 
 // This is the chainId your dApp will work on.
-const activeChainId = ChainId.Goerli;
+const activeChainId = ChainId.Mumbai;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
