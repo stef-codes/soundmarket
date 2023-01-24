5d75836c7d138d18030ffc3cea6b26191386e56c
Stefanie <stefcodes@gmail.com>
2023-01-23 20:15:11 -0700

    change address

--- addresses.ts
+++ addresses.ts
@@ -1,2 +1,3 @@
 // Use your marketplace contract address here
-export const marketplaceContractAddress = '0xf7ba7cd986d5bC960433697Ca8cF9e7101F3c017';
+// export const marketplaceContractAddress = '0xf7ba7cd986d5bC960433697Ca8cF9e7101F3c017';
+export const marketplaceContractAddress = '0x7653Cd64320c65733C005EF855CdE916705B483D';
